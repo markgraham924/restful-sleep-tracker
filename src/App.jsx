f6269cb8ae7f87fc55c6740dafac6a750e0589b9
Khalid <kabuafifeh19@gmail.com>
2025-03-06 21:48:54 +0000

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import SleepClock from "./pages/SleepClock"; // Import the new component
 
 const App = () => {
   return (
@@ -11,12 +12,15 @@ const App = () => {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/sleep-clock">Sleep Clock</Link></li> 
+
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/sleep-clock" element={<SleepClock />} /> 
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
---
+++ src/Sleep Clock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SleepClock = () => {
+  return (
+    <div className="container">
+      <h1>Sleep Clock</h1>
+      <p>This page will display sleep tracking information.</p>
+    </div>
+  );
+};
+
+export default SleepClock;
